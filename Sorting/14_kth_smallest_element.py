e3d33d1d7a586fb95e7d7bb95f5b05f8f0fa643d
jaideep6214 <43785047+jaideep6214@users.noreply.github.com>
2021-04-14 19:57:36 +0530

    Update 14_kth_smallest_element.py

--- Sorting/14_kth_smallest_element.py
+++ Sorting/14_kth_smallest_element.py
@@ -1,32 +1,29 @@
 import random
 
-def hoare_partition(arr,l,h):
-    pivot=arr[l]
+def lomuto_partition(arr,l,h):
+    pivot=arr[h]
     i=l-1
-    j=h+1
-    while (True):
-        i+=1
-        while (arr[i]<pivot):
+
+    for j in range(l,h):
+        if (arr[j]<pivot):
             i+=1
-        j-=1
-        while(arr[j]>pivot):
-            j-=1
-        if (i>=j):
-            return j
-        arr[i],arr[j]=arr[j],arr[i]
+            arr[i],arr[j]=arr[j],arr[i]
+    arr[h],arr[i+1]=arr[i+1],arr[h]
+    return i+1   
+
 
 
 def random_partition(arr,l,h):
     x=random.randint(l,h)
     arr[l],arr[x]=arr[x],arr[l]
-    return hoare_partition(arr,l,h)
+    return lomuto_partition(arr,l,h)
 
 def kth_Smallest(arr,n,k):
     l=0
     r=n-1
     while(l<=r):
-        print(arr)
         p=random_partition(arr,l,r)
+        print (arr,p)
         if (p==k-1):
             return p
         elif(p>k-1):
@@ -37,4 +34,4 @@ def kth_Smallest(arr,n,k):
 
 arr=[10,4,5,8,11,6,26]
 ans=kth_Smallest(arr,len(arr),k=5)
-print(ans)
+print(arr[ans]," is the element");
